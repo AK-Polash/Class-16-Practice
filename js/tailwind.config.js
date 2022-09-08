9cf98133f07c093f157d5672687a1b523aa1d976
AK-Polash <polashk199@gmail.com>
2022-09-09 05:46:56 +0600

    Header added by polash

---
+++ js/tailwind.config.js
@@ -0,0 +1,20 @@
+tailwind.config = {
+  theme: {
+    extend: {
+      colors: {
+        primary: "#F40404",
+        secondary: "#F0F0F0",
+        white_light: "rgba(255, 255, 255, 0.5)",
+        yellow_line: "#FFB800",
+        gray: "#282828",
+        light_gray: "#5C6A92"
+      },
+      fontFamily: {
+        my_font: ['Poppins', 'sans-serif']
+      },
+      maxWidth: {
+        container: '1140px',
+      }
+    }
+  },
+};
